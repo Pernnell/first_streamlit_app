3cc081fd1cfde8b2624bbed3c198b44b4c9954c9
Pernnell <136927077+Pernnell@users.noreply.github.com>
2023-06-18 02:09:19 +0530

    Update streamlit_app.py
    removed all fruits

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,8 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 # streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 # streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries,'])
 # fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
-fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries', 'Banana'])
+# fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries', 'Banana'])
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), [])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
