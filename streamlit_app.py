4ae8ef28b32fcf5660f4aa707e5f7cda76715150
Pernnell <136927077+Pernnell@users.noreply.github.com>
2023-06-26 00:54:00 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,7 +39,7 @@ streamlit.dataframe(fruits_to_show)
 #LESSON 9:
 #new section to display fruityvice api  response
 
-streamlit.header("Fruityvice Fruit Advice!")
+#streamlit.header("Fruityvice Fruit Advice!")
 
 #--ADDED AT THE END --
 #Add a Text Entry Box and Send the Input to Fruityvice as Part of the API Call
@@ -64,7 +64,7 @@ streamlit.header("Fruityvice Fruit Advice!")
 #new section to display fruityvice api  response
 streamlit.header("Fruityvice Fruit Advice!")
 try:
-  fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
     streamlit.write('The user entered ', fruit_choice)
   else:
