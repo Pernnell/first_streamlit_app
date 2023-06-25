18a098b0e3d3807bce3475828d2c206d1b27908b
Pernnell <136927077+Pernnell@users.noreply.github.com>
2023-06-26 01:09:05 +0530

    Update streamlit_app.py
    Create a Function

--- streamlit_app.py
+++ streamlit_app.py
@@ -60,21 +60,45 @@ streamlit.dataframe(fruits_to_show)
 
 # REPLACEMENT FOR LESSON 9 PART
 
-#LESSON 12:
+#LESSON 12 PART 1:
+#new section to display fruityvice api  response
+# streamlit.header("Fruityvice Fruit Advice!")
+# try:
+#   fruit_choice = streamlit.text_input('What fruit would you like information about?')
+#   if not fruit_choice:
+#     streamlit.error("Please select a fruit to get information. ")
+#     #streamlit.write('The user entered ', fruit_choice)
+#   else:
+#     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+#     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+#     streamlit.dataframe(fruityvice_normalized)
+# 
+# except URLError as e:
+#   streamlit.error()
+
+# REPLACEMENT FOR LESSON 12 PART-1
+
+#LESSON 12 PART 2:
+
+#create the repeatable code block (called a functiom)
+def get_fruityvice_data(this_fruit_choice):
+      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+      return fruityvice_normalized
+
 #new section to display fruityvice api  response
 streamlit.header("Fruityvice Fruit Advice!")
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
     streamlit.error("Please select a fruit to get information. ")
-    #streamlit.write('The user entered ', fruit_choice)
   else:
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    streamlit.dataframe(fruityvice_normalized)
+    back_from_function = get_fruityvice_data(fruit_choice)
+    streamlit.dataframe(back_from_function)
 
 except URLError as e:
   streamlit.error()
+
 
 
 # import snowflake.connector
